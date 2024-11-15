0253d952e1438a20322e7b2352815769b38ed8df
fazilcmohammed <mohammedfazilc786@gmail.com>
2024-11-15 21:40:52 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <h1>Fazil.</h1>
+            <ul>
+                <li><a href="#">About Me</a></li>
+                <li><a href="#">Skills</a></li>
+                <li><a href="#">Project</a></li>
+                <li><a href="#">Contact Me</a></li>
+            </ul>
+            <div>
+                
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
